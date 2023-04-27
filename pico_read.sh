852c9584e2d26e41ed5050e65b7af525640c0de1
loris <loris.giann@live.it>
2023-04-27 15:31:00 +0200

    loop pico module
    remove prints and loop the read pico module to run forever bb

--- pico_read.sh
+++ pico_read.sh
@@ -1,8 +1,16 @@
 SCR_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
 echo $SCR_DIR
-args=$(timeout 1s cat /dev/ttyACM0)
-echo "${args} read"
-args=$(echo $args | sed 's/,/ /g')
-echo "${args} modified "
-${SCR_DIR}/pub_values.sh ${args}
-echo done
+
+while [ true ]; do
+	# Read device for commas separated values
+	args=$(timeout 1s cat /dev/ttyACM0)
+	#echo "${args} read"
+	
+	# Replace commas with spaces
+	args=$(echo $args | sed 's/,/ /g')
+	#echo "${args} modified "
+	
+	# Feed into publish script (uses each value as an arguement)
+	${SCR_DIR}/pub_values.sh ${args}
+	#echo done
+done
