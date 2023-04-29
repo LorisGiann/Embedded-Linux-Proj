3b0766eebe4d4a238d380f1d78a14efdb8e86f86
loris <loris.giann@live.it>
2023-04-29 11:01:46 +0200

    water_request_gate.sh complete

---
+++ water_request_gate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PICO_DEVICE_FILE=devttyACM0
+
+PUMP_REQ_TOPIC=dev0/pump_req
+PUMP_ALARM_TOPIC=dev0/pump_alarm
+
+
+#local variables
+PUMP_ALARM="0" #suppose no alarm at system startup
+
+mosquitto_sub -h localhost -p 1883 -t $PUMP_REQ_TOPIC -t $PUMP_ALARM_TOPIC -d -u pi -P raspberry -F "%t %p" | grep -vE --line-buffered '^Client|^Subscribed' | while read TOPIC MESSAGE 
+do 
+	#echo "From \"$TOPIC\": \"$MESSAGE\""
+
+	if [[ $TOPIC == $PUMP_ALARM_TOPIC ]] ; then
+		if [[ $MESSAGE == "0" || $MESSAGE == "1" ]] ; then
+			PUMP_ALARM=$MESSAGE
+			echo "Setting PUMP_ALARM to $PUMP_ALARM"
+		else
+			echo "ERROR: invalid message in $PUMP_ALARM_TOPIC" | tee /dev/stderr 
+		fi
+	elif [[ $TOPIC == $PUMP_REQ_TOPIC ]] ; then
+		if [[ $PUMP_ALARM == "0" ]] ; then
+			echo "Pump request ACCEPTED"
+			echo "p" > $PICO_DEVICE_FILE
+		else
+			echo "Pump request REJECTED"
+		fi
+	else
+		echo "ERROR: not recognized topic!" | tee /dev/stderr 
+	fi
+		
+done
