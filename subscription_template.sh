dcbc0868926f621d20f3bdbd20485e4108c00f67
loris <loris.giann@live.it>
2023-04-29 23:13:15 +0200

    some more structure to template

--- subscription_template.sh
+++ subscription_template.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
-mosquitto_sub -h localhost -p 1883 -t dev0/button -t dev0/pump_alarm -d -u pi -P raspberry -F "%t %p" | grep -vE --line-buffered '^Client|^Subscribed' | while read TOPIC MESSAGE 
+
+#constants / definitions
+DEV=dev0
+
+
+#topics to subscribe
+__1_TOPIC=$DEV/__topic_1__
+__2_TOPIC=$DEV/__topic_2__
+
+#topics to write to
+OUT_TOPIC=$DEV/__topic_3__
+
+#local variables (maintain state between messages)
+
+
+mosquitto_sub -h localhost -p 1883 -t $__1_TOPIC -t $__2_TOPIC -d -u pi -P raspberry -F "%t %p" | grep -vE --line-buffered '^Client|^Subscribed' | while read TOPIC MESSAGE 
 do 
-	echo $TOPIC
-	echo $MESSAGE
+	#echo "From \"$TOPIC\": \"$MESSAGE\""
+
+	if [[ $TOPIC == $__1_TOPIC ]] ; then
+                ...
+		./mqtt_pub.sh $OUT_TOPIC "Req($0)"
+		...
+        elif [[ $TOPIC == $__2_TOPIC ]] ; then
+        	...
+		./mqtt_pub.sh $OUT_TOPIC "Req($0)"
+		...
+	else
+                echo "ERROR: not recognized topic!" | tee /dev/stderr
+        fi
 done
